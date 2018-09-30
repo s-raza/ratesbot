124432e48baef6f62f9875327efcf90b5418b59a
s-raza <raza.salman@gmail.com>
2018-09-30 10:35:55 +0400

    Updated copyright & license info

--- RatesBot/DB/RatesDB.py
+++ RatesBot/DB/RatesDB.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python
+# 
+# Simple telegram bot to track the rates of tradeables available on the web (e.g. Gold),
+# and send notifications on Telegram if the rate of a tradeable changes.
+# Copyright (C) 2018
+# Salman Raza <raza.salman@gmail.com>
+#
+# GNU General Public License v3.0
+#
+# This program is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>
+
 from RatesBot.DB.Models import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
